c947ad7254a7a71c900da314f722ac362d0b1364
d4rkstrife <p.gdc85@gmail.com>
2020-05-09 16:30:08 +0200

    version finale

--- Citation.js
+++ Citation.js
@@ -10,25 +10,29 @@ class Citation {
             this.generer();
         });
     }
-    chiffreAleatoire(max) {
+    chiffreAleatoire(max) {             //fonction qui crée un chiffre aléatoire
         return Math.floor(Math.random() * max)
     }
     generer() {
-        this.reset();
-        for (let i = 0; i < this.choixNbr(); i++) {
-            let phrase = `${this.selAlea(this.selType())}`
+        this.reset();   // on efface les phrases précédents
+        for (let i = 0; i < this.choixNbr(); i++) { //boucle sur le nombre choisi   
+            let phrase = `${this.construPhrase()}`
             this.creerElt(phrase)
         }
-        console.log(this.choixNbr())
     }
-    selAlea(type) {
-        return type.debutPhrase[this.chiffreAleatoire(type.debutPhrase.length)] + " " + type.milieuPhrase[this.chiffreAleatoire(type.milieuPhrase.length)] + " " + type.finPhrase[this.chiffreAleatoire(type.finPhrase.length)];
+    construPhrase() {       //fonction qui met la phrase bout à bout
+        let x = this.data[this.selType()].length; //récupère le nombre de bout de phrase qui existe
+        let phrase = ""
+        for (let i = 0; i < x; i++) { //boucle qui assemble les bouts de phrase.
+            phrase = phrase + " " + this.data[this.selType()][i][this.chiffreAleatoire(this.data[this.selType()][i].length)];
+        }
+        return phrase;
     }
-    selType() {
-        let x = eval("this.data." + this.typeCitation.value);
+    selType() {     //fonction qui récupère le type de citations à afficher
+        let x = eval(this.typeCitation.value);
         return x
     }
-    choixNbr() {
+    choixNbr() {    //fonction qui récupère le nombre de citations à afficher
         for (var i = 0; i < this.radio.length; i++) {
             if (this.radio[i].checked) {
                 this.nbrCitation = this.radio[i].value;
@@ -36,29 +40,30 @@ class Citation {
         }
         return this.nbrCitation
     }
-    creerElt(phrase) {
+    creerElt(phrase) {  //crée un element p dans la div choisie.
         let newElt = document.createElement("p");
         newElt.className = "citation";
         this.elt.appendChild(newElt);
         newElt.textContent = phrase;
     }
-    reset() {
+    reset() {       //efface les elements affichés lors du générer précedent.
         while (this.elt.firstChild) {
             this.elt.removeChild(this.elt.firstChild);
         }
     }
 
 }
-const data = {
-    phrase1: {
-        debutPhrase: ["Il ne faut jamais", "Pourquoi", "On peut toujours", "Il faut être prêt à", "On adore", "On préfère éviter de"],
-        milieuPhrase: ["tuer un ours", "courir après une pierre", "lacher une caisse", "s'endormir", "marcher à pieds joints"],
-        finPhrase: ["quand on court.", "face au vent.", "quand on a bu.", "à l'aveugle.", "par inadvertance.", "quand on ne s'y attend pas.", "sans le vouloir.", "sans faire attention."]
-    },
-    phrase2: {
-        debutPhrase: ["Tintin", "Le capitaine Haddock", "Titeuf", "Asterix", "Lucky Luke"],
-        milieuPhrase: ["court après", "mange", "tape sur", "tire plus vite qu'", "crie sur"],
-        finPhrase: ["un sanglier.", "une banane.", "un romain.", "un poisson.", "une ombre.", "un nuage.", "un menhir."]
-    }
-};
+const data = [
+    [
+        ["Il ne faut jamais", "Pourquoi", "On peut toujours", "Il faut être prêt à", "On adore", "On préfère éviter de"],
+        ["tuer un ours", "courir après une pierre", "lacher une caisse", "s'endormir", "marcher à pieds joints"],
+        ["quand on court.", "face au vent.", "quand on a bu.", "à l'aveugle.", "par inadvertance.", "quand on ne s'y attend pas.", "sans le vouloir.", "sans faire attention."]
+    ],
+    [
+        ["Tintin", "Le capitaine Haddock", "Titeuf", "Asterix", "Lucky Luke"],
+        ["court après", "mange", "tape sur", "tire plus vite qu'", "crie sur"],
+        ["un sanglier.", "une banane.", "un romain.", "un poisson.", "une ombre.", "un nuage.", "un menhir."]
+    ]
+];
+
 let citation = new Citation(data, "button", "citation-container", "type", "nombre");
